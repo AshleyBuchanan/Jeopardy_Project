e78f981e38b903ff936c817e42d1170f7b6a899a
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-05-03 16:12:28 -0700

    added basic MVC (model is api schema).

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const ejs = require('ejs');
+const axios = require('axios');
 
-const url = 'https://projects.springboard.com/jeopardy/api/';
+//that was a dirty trick with the text not matching the link ;-D
+const url = 'https://rithm-jeopardy.herokuapp.com/api/';
 const port = 3000;
+
+const availableIds = [];
+const chosenIds = new Set();
+let data = [];
+
+(async function () {
+    try {
+        const response = await axios.get(url + 'categories?count=2000', {
+            headers: { 'User-Agent': 'Axios/1.0' }
+        });
+        await response.data.forEach(element => {
+            availableIds.push(element.id);
+        });
+    } catch (err) {
+        console.log('error');
+    }
+})();
+
+async function getCategories() {
+
+    console.log('-------');
+
+    while (data.length < 6) {
+        const id = availableIds[Math.floor(Math.random() * availableIds.length)];
+        if (!chosenIds.has(id)) {
+            try {
+                const response = await axios.get(url + `category?id=${id}`, {
+                    headers: { 'User-Agent': 'Axios/1.0' }
+                });
+
+                //taking only the data that I want
+                const obj = {
+                    id: response.data.id,
+                    title: response.data.title,
+                    clues: [],
+                };
+
+                for (let i = 0; i < response.data.clues.length; i++) {
+                    obj.clues.push({
+                        question: response.data.clues[i].question,
+                        answer: response.data.clues[i].answer,
+                    });
+                }
+
+                chosenIds.add(id);
+                data.push(obj);
+
+            } catch (err) {
+                console.log('error', err);
+            }
+        }
+    }
+};
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,6 +67,14 @@ app.set('view engine', 'ejs');
 
 app.get('/jeopardy/', (req, res) => {
     res.render('jeopardy');
+});
+
+app.get('/jeopardy/categories', async (req, res) => {
+    data = [];
+    chosenIds.clear();
+    await getCategories().then(() => {
+        res.json(data);
+    });
 });
 
 app.listen(port, () => console.log(`Jeopardy_Server listening on port:${port}`));
--- public/jeopardy.js
+++ public/jeopardy.js
@@ -1,6 +1,61 @@
 const blocks = document.querySelectorAll('.game-cube');
-const categoryBlocks = [];
-const answerBlocks = [];
-for (let i = 0; i < 36; i++) {
-    i < 6 ? categoryBlocks.push(blocks[i]) : answerBlocks.push(blocks[i]);
-}
+
+// to encapsulate data
+(async function () {
+    const categoryBlocks = [];
+    const answerBlocks = [];
+    let data = [];
+
+    function addDisplayText(selectable, block, data, count) {
+        console.log(data);
+        switch (selectable) {
+            case true:
+                setTimeout(() => {
+                    block.innerText = '?';
+                    block.classList.add('clues');
+                    block.dataset.question = data.clues[count].question;
+                    block.dataset.answer = data.clues[count].answer;
+                    answerBlocks.push(block);
+                }, 1800 + (100 * count));
+
+                block.addEventListener('click', (event) => {
+                    console.log(block.dataset.answer);
+                    block.innerText = block.dataset.question;
+                    block.classList.remove('clues');
+                    block.classList.add('questions');
+                });
+                break;
+
+            case false:
+                setTimeout(() => {
+                    block.innerText = data;
+                    block.classList.add('categories');
+                    categoryBlocks.push(block);
+                }, 200 * count);
+                break;
+        }
+    }
+
+    async function getData() {
+        try {
+            data = [];  //clear it out;
+            const res = await fetch('/jeopardy/categories');
+            if (!res.ok) throw new Error('Fetch failed');
+            data = await res.json();
+            console.log(data);
+            for (let i = 0; i < 6; i++) {
+                addDisplayText(false, blocks[i], await data[i].title, i);
+            }
+            for (let x = 0; x < 6; x++) {
+                for (let y = 0; y < 5; y++) {
+                    addDisplayText(true, blocks[x + (y * 6) + 6], await data[x], y);
+                }
+            }
+        } catch (err) {
+            console.error('Error:', err);
+        }
+    }
+    getData();
+})();
+
+
